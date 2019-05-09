c9419623716143c7e19155a1ed52afa44bde0a29
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 01:43:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,3 +4,10 @@ def my_each(array)
   i += 1 
   end
 end
+def my_each(array)
+  my_statements = []
+  toppings.each do |topping|
+    my_statements << "I love #{topping} on my burgers!"
+  end
+  my_statements
+end
