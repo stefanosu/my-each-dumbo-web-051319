b87d52205e25aab432a0dc1ddc5fc5196526267d
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-09 02:03:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,3 +5,14 @@ def my_each(array)
     i = i + 1
   end
 end
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
